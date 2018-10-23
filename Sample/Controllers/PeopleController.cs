77235b7dd709e8ed142ea12f53dfd7f9a7d9a177
Hector Carballo <hectorcarballo.hco@gmail.com>
2018-10-23 01:03:42 -0400

    adding sample project

---
+++ Sample/Controllers/PeopleController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Qurl.Abstractions;
+using Sample.Models;
+
+namespace Sample.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/People")]
+    public class PeopleController : Controller
+    {
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(new Person
+            {
+                Id = id
+            });
+        }
+
+        [HttpGet]
+        public IActionResult Get([FromQuery]Query<PersonFilter> query)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Sample/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using Qurl.Abstractions;
+using System;
+
+namespace Sample.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public bool Active { get; set; }
+    }
+
+    public class PersonFilter
+    {
+        public FilterProperty<int> Id { get; set; }
+        public FilterProperty<string> Name { get; set; }
+        public FilterProperty<DateTime> Birthday { get; set; }
+        public FilterProperty<bool> Active { get; set; }
+    }
+}
