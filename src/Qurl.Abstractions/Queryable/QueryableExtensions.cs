75b2afee20b9703c0c29cad4a3c55df22cc85758
Hector Carballo <hectorcarballo.hco@gmail.com>
2018-10-26 13:17:20 -0400

    renaming method. updating package version

--- src/Qurl.Abstractions/Queryable/QueryableExtensions.cs
+++ src/Qurl.Abstractions/Queryable/QueryableExtensions.cs
@@ -185,16 +185,16 @@ namespace Qurl.Abstractions.Queryable
 
     public static class QueryableExtensions
     {
-        public static IQueryable<TModel> ApplyQuery<TModel, TFilter>(this IQueryable<TModel> source, Query<TFilter> query, bool applySort = true)
+        public static IQueryable<TModel> ApplyQuery<TModel, TFilter>(this IQueryable<TModel> source, Query<TFilter> query, bool applySortAndPaging = true)
             where TModel : class
             where TFilter : new()
         {
             var queryableHelper = new QueryableHelper<TModel, TFilter>(query);
 
-            return queryableHelper.GetQueryable(source, applySort);
+            return queryableHelper.GetQueryable(source, applySortAndPaging);
         }
 
-        public static IQueryable<TModel> ApplySort<TModel, TFilter>(this IQueryable<TModel> source, Query<TFilter> query)
+        public static IQueryable<TModel> ApplySortAndPaging<TModel, TFilter>(this IQueryable<TModel> source, Query<TFilter> query)
             where TModel : class
             where TFilter : new()
         {
