71d336c3c4810f0327fdc1b036595f98bfdb02dc
Hector Carballo <hectorcarballo.hco@gmail.com>
2022-08-25 23:32:01 -0400

    adding project with aspnetcore extensions and updating versions

---
+++ src/Qurl.AspNetCore/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Qurl
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddQurl(this IServiceCollection services)
+        {
+            services.AddSingleton<FilterFactory>();
+            services.AddSingleton<QueryBuilder>();
+        }
+    }
+}
